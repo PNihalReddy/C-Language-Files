332192f776f3125a662e50d61ee5f6acacd15bf2
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-03-04 09:23:58 +0530

    Add files via upload

---
+++ Product.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("Enter two numbers to multiply : ");
+	scanf("%d %d",&a,&b);
+	
+	int p=a*b;
+	printf("The product is : %d\n",p);
+	return 0;
+}
