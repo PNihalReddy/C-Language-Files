773db8e66c3de63b34b3d6b813f20aabc88e59c9
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-03-05 09:49:56 +0530

    Add files via upload

---
+++ Cube of a Number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int a;
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	if (a%2==0)
+	{
+	printf("The number is even");}
+	else{
+			printf("The number is odd");
+	}
+}
+	
+
