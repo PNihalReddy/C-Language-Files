66a728af32a6841a8218ebe9f8f8653830459036
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-03-06 10:51:09 +0530

    Add files via upload

---
+++ Sum of Digits.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+	int n,digit,sum=0;
+	printf("Enter the number:");
+	scanf("%d",&n);
+	while(n!=0){
+		digit=n%10;
+		sum=sum+digit;
+		n=n/10;
+	}
+	printf("The sum of the digits is:%d",sum);
+}
