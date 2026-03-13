1cde1050b32c072453c8e6cf857e5017b45869b3
Pakkireddy Nihal Reddy <pnihalnr@gmail.com>
2026-03-13 21:58:06 +0530

    Add files via upload

---
+++ PRac.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the number");
+	scanf("%d",&n);
+	if (n>=1){
+		printf("The number is natural");
+	}
+	else{
+		printf("The number is whole");
+	}
+	return 0;
+}
